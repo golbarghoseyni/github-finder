3e1947564d1c665ca0c003fb8e180aa1b6ec3cab
Seyedehgolbarg Hossininezhad Khorasgani <seyedehgolbarg.hossininezhad-khorasgani@etu.u-pec.fr>
2021-11-03 21:09:30 +0100

    l12

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './users/Users';
+import axios from 'axios'
 
 
 class App extends Component {
+  state = {
+    users : [],
+    loading : false 
+  }
+
+  async componentDidMount(){
+    this.setState({loading : true})
+
+    const res = await axios.get('https://api.github.com/users')
+    
+    this.setState({users : res.data , loading :false})
+  }
+
+
+
   render(){
-   
     return (
       <div className="App">
        <Navbar />
        <div className="container">
-       <Users />
+       <Users loading={this.state.loading} users={this.state.users}/>
       </div>
       </div>
     );
--- src/users/UserItem.js
+++ src/users/UserItem.js
@@ -2,7 +2,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const UserItem = ({user :{login , avatar_url , html_url }}) => {
-  
 
     return (
       <div className ='card text-center'>
@@ -13,7 +12,6 @@ const UserItem = ({user :{login , avatar_url , html_url }}) => {
             More
             </a>
             </div>
-     
       </div>
     )
 }
