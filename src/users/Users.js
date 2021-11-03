c1644cb6d8827df3ec400ee867bc43544ff3fc14
Seyedehgolbarg Hossininezhad Khorasgani <seyedehgolbarg.hossininezhad-khorasgani@etu.u-pec.fr>
2021-11-03 21:50:24 +0100

    l14

--- src/users/Users.js
+++ src/users/Users.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../components/layout/Spinner'
 
-class Users extends Component {
- 
-  render() {
+const Users = ({users , loading}) => {
+
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div style={userStyle}>
-        {this.props.users.map(user => (
-          <UserItem key={user.id}  user={user} />
+        {users.map(user => (
+          <UserItem key={user.id} user={user} />
         ))}
-        
       </div>
-    )
+    );
   }
-}
+};
 
 const userStyle = {
   display: 'grid',
